5fb6dab8d45a59242417c97ce757be037c4af4a7
Imaweasle <lydiaschieffer@gmail.com>
2025-09-04 21:09:48 -0500

    Updated khinsiderScrapper and have the start of a QT design

---
+++ defs.h
@@ -0,0 +1,8 @@
+#include <QApplication>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QMainWindow>
+#include <QMenu>
+#include <QLabel>
+#include <QVBoxLayout>
+
--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
-#include <iostream>
+#include "defs.h"
+#include "mainWindow.h"
 
-int main() {}
+int main(int argc, char **argv)
+{
+ QApplication app (argc, argv);
+
+ //Start the main window
+ MainWindow window;
+
+ window.show();
+
+ return app.exec();
+}
